dfe69ba68442c66dccf08032c20ffbe3a5ae8675
Stefan Cotos <stefancotos8@gmail.com>
2024-06-22 02:33:30 +0300

    Add database for Heroku

--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php
 
 return array(
-    'servername' => "localhost:3307",
-    'username' => "root",
-    'password' => '',
-    'db' => 'web_project'
+    'servername' => "eu-cluster-west-01.k8s.cleardb.net:3306",
+    'username' => "b60cd745ed6912",
+    'password' => '13552fad',
+    'db' => 'heroku_0a0d867574772ff'
 );
