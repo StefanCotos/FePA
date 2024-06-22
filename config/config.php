af3705b6132d6e65d14182b81f3294f6318826f9
Stefan Cotos <stefancotos8@gmail.com>
2024-06-22 11:51:39 +0300

    Some details

--- config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return array(
-    'servername' => "eu-cluster-west-01.k8s.cleardb.net:3306",
-    'username' => "b60cd745ed6912",
-    'password' => '13552fad',
-    'db' => 'heroku_0a0d867574772ff'
-);
