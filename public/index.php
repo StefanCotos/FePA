aa04a881cf365ec428f104bef424e70fcd55c624
Stefan Cotos <stefancotos8@gmail.com>
2024-06-22 03:21:31 +0300

    After merge (some things need to do)

--- public/index.php
+++ public/index.php
@@ -3,7 +3,6 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-
 use controllers\UserController;
 use controllers\ReportController;
 use database\Database;
