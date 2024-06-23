f1cfaae41d9213de02473389a8702957a3f4b867
Stefan Cotos <stefancotos8@gmail.com>
2024-06-23 15:00:33 +0300

    No image option

--- public/scripts/see_image.js
+++ public/scripts/see_image.js
@@ -1,17 +1,22 @@
 function see_image(image, reportId) {
     $.ajax({
-        url: window.location.origin + "/Web_Project/public/index.php/image/"+reportId,
+        url: window.location.origin + "/Web_Project/public/index.php/image/" + reportId,
         type: "GET",
         dataType: "json",
         success: function (data) {
             console.log(data);
-            let html_append = "";
-            $.each(data, function (index, item) {
-                html_append +=
-                    "<img id='news-image' src='" + item.name + "' alt='animals'>";
-                return false;
-            });
-            $("#"+image).html(html_append);
+            if ($.isEmptyObject(data)) {
+                let html_append = "<img id='news-image' src='https://res.cloudinary.com/hf0egkogn/image/upload/v1719143533/33.jpg' alt='animals'>";
+                $("#" + image).html(html_append);
+            } else {
+                let html_append = "";
+                $.each(data, function (index, item) {
+                    html_append +=
+                        "<img id='news-image' src='" + item.name + "' alt='animals'>";
+                    return false;
+                });
+                $("#" + image).html(html_append);
+            }
         },
         error: function (xhr, status, error) {
             console.error("Error: " + error);
--- public/scripts/see_images.js
+++ public/scripts/see_images.js
@@ -1,17 +1,22 @@
 function see_images(reportId) {
     $.ajax({
-        url: window.location.origin + "/Web_Project/public/index.php/image/"+reportId,
+        url: window.location.origin + "/Web_Project/public/index.php/image/" + reportId,
         type: "GET",
         dataType: "json",
         success: function (data) {
             console.log(data);
-            let html_append = "";
-            $.each(data, function (index, item) {
-                html_append +=
-                    "<img class='mySlides' id='news-image' src='" + item.name + "' alt='animals'>";
-            });
-            $("#images").html(html_append);
-            carousel();
+            if ($.isEmptyObject(data)) {
+                let html_append = "<img id='news-image' src='https://res.cloudinary.com/hf0egkogn/image/upload/v1719143533/33.jpg' alt='animals'>";
+                $("#images").html(html_append);
+            } else {
+                let html_append = "";
+                $.each(data, function (index, item) {
+                    html_append +=
+                        "<img class='mySlides' id='news-image' src='" + item.name + "' alt='animals'>";
+                });
+                $("#images").html(html_append);
+                carousel();
+            }
         },
         error: function (xhr, status, error) {
             console.error("Error: " + error);
